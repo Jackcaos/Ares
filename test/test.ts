26ee0973ba7e5e6f83e89a89663bd978e4893e70
-–global <--global>
2024-04-26 20:51:38 +0800

    fix: 删除无用代码

--- test/test.ts
+++ test/test.ts
@@ -1,11 +1,9 @@
 import { before } from "../src/web/route-mapping.decorator";
 import User from "./user";
 
-// @Controller("/")
 class Aop {
   @before(User, "getUser")
   public getAop(aaa: string) {
     console.log("get aop after");
-    // return "aaa";
   }
 }
